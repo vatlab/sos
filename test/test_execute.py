db460962b8d7544a4ad90a78bfd7dc7bf0384655
Bo Peng <ben.bog@gmail.com>
2016-05-04 12:33:42 -0500

    Fix wrongfully committed test

--- test/test_execute.py
+++ test/test_execute.py
@@ -26,6 +26,7 @@ from __future__ import unicode_literals
 
 import os
 import time
+import glob
 import unittest
 import shutil
 
@@ -1060,12 +1061,64 @@ touch temp/${ff}
         #
         shutil.rmtree('temp')
 
+    def testUseOfRunmode(self):
+        '''Test the use of run_mode variable in SoS script'''
+        #
+        if os.path.isdir('temp'):
+            shutil.rmtree('temp')
+        os.mkdir('temp')
+        #
+        env.sig_mode = 'ignore'
+        script = SoS_Script('''
+[1: alias='res']
+import random
+for i in range(3):
+    with open('temp/test_${random.randint(1, 100000)}.txt', 'w') as res:
+        res.write(str(i))
+''')
+        wf = script.workflow()
+        #
+        for run_mode in ('dryrun', 'prepare', 'run'):
+            env.run_mode = run_mode
+            Sequential_Executor(wf).run()
+        # we should have 9 files
+        files = glob.glob('temp/*.txt')
+        self.assertEqual(len(files), 9)
+        #
+        # now, if we strict to run mode, we should be fine
+        #
+        #
+        if os.path.isdir('temp'):
+            shutil.rmtree('temp')
+        os.mkdir('temp')
+        env.sig_mode = 'ignore'
+        script = SoS_Script('''
+[1: alias='res']
+import random
+if run_mode == 'run':
+    for i in range(3):
+        with open('temp/test_${random.randint(1, 100000)}.txt', 'w') as res:
+            res.write(str(i))
+
+''')
+        wf = script.workflow()
+        for run_mode in ('dryrun', 'prepare', 'run'):
+            env.run_mode = run_mode
+            Sequential_Executor(wf).run()
+        # we should have 9 files
+        files = glob.glob('temp/*.txt')
+        self.assertEqual(len(files), 3)
+
     def testActiveActionOption(self):
         '''Test the active option of actions'''
+        if os.path.isdir('temp'):
+            shutil.rmtree('temp')
+        os.mkdir('temp')
+        #
+        script = SoS_Script('''
 [1]
 rep = range(5)
 input: for_each = 'rep'
-output: 'temp/${_rep}.txt'
 # ff should change and be usable inside run
 ff = '${_rep}.txt'
 run:  active=0
@@ -1074,6 +1127,8 @@ touch temp/${ff}
 ''')
         wf = script.workflow()
         Sequential_Executor(wf).run()
+        files = glob.glob('temp/*.txt')
+        self.assertEqual(len(files), 1)
         #
         shutil.rmtree('temp')
 
