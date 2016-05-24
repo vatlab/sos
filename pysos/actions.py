0c11245efe44b9083848df2470b5e5f5d81f94d5
Bo Peng <ben.bog@gmail.com>
2016-05-24 12:33:34 -0500

    Fix a bug where parameter transcript is not passed to nested workflow

--- pysos/actions.py
+++ pysos/actions.py
@@ -499,7 +499,7 @@ def sos_run(workflow, source={}):
     # recusive nested workflow and should not be allowed
     if env.sos_dict['step_name'] in ['{}_{}'.format(x.name, x.index) for x in wf.sections if not x.is_parameters]:
         raise RuntimeError('Nested workflow {} contains the current step {}'.format(workflow, env.sos_dict['step_name']))
-    return Sequential_Executor(wf).run(args=env.sos_dict['__args__'], nested=True,
+    return Sequential_Executor(wf, transcript=env.sos_dict['__transcript__']).run(args=env.sos_dict['__args__'], nested=True,
         run_mode=env.run_mode, sig_mode=env.sig_mode, verbosity=env.verbosity)
 
 @SoS_Action(run_mode=['inspect', 'prepare', 'run'])
