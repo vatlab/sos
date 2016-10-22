d50ef9d4f5b145fa2078df45160147d78b56f749
Bo Peng <ben.bog@gmail.com>
2016-10-22 00:13:50 -0500

    First success test of Jupyter kernel

--- test/test_kernel.py
+++ test/test_kernel.py
@@ -31,58 +31,38 @@ import unittest
 from contextlib import contextmanager
 from pysos.kernel import SoS_Kernel
 from jupyter_client import manager
+from ipykernel.tests.utils import assemble_output, start_new_kernel, flush_channels, stop_global_kernel, execute
 
-from subprocess import STDOUT
 import os
-import nose as nt
 import atexit
+import nose
+import nose.tools as nt
 
 KM = None
 KC = None
 
 @contextmanager
-def kernel():
+def sos_kernel():
     """Context manager for the global kernel instance
     Should be used for most kernel tests
     Returns
     -------
     kernel_client: connected KernelClient instance
     """
-    yield start_global_kernel()
+    yield start_sos_kernel()
 
 
-def start_new_kernel(**kwargs):
-    """start a new kernel, and return its Manager and Client
-    Integrates with our output capturing for tests.
-    """
-    try:
-        stdout = nt.iptest_stdstreams_fileno()
-    except AttributeError:
-        stdout = open(os.devnull)
-    kwargs.update(dict(stdout=stdout, stderr=STDOUT))
-    return manager.start_new_kernel(startup_timeout=10, **kwargs)
-
-
-def start_global_kernel():
+def start_sos_kernel():
     """start the global kernel (if it isn't running) and return its client"""
     global KM, KC
     if KM is None:
-        KM, KC = start_new_kernel()
-        atexit.register(stop_global_kernel)
+        KM, KC = start_new_kernel(kernel_name='sos')
+        atexit.register(stop_sos_kernel)
     else:
         flush_channels(KC)
     return KC
 
-def uses_kernel(test_f):
-    """Decorator for tests that use the global kernel"""
-    def wrapped_test():
-        with kernel() as kc:
-            test_f(kc)
-    wrapped_test.__doc__ = test_f.__doc__
-    wrapped_test.__name__ = test_f.__name__
-    return wrapped_test
-
-def stop_global_kernel():
+def stop_sos_kernel():
     """Stop the global shared kernel instance, if it exists"""
     global KM, KC
     KC.stop_channels()
@@ -92,35 +72,16 @@ def stop_global_kernel():
     KM.shutdown_kernel(now=True)
     KM = None
 
-def execute(code='', kc=None, **kwargs):
-    """wrapper for doing common steps for validating an execution request"""
-    #from .test_message_spec import validate_message
-    if kc is None:
-        kc = KC
-    msg_id = kc.execute(code=code, **kwargs)
-    reply = kc.get_shell_msg(timeout=10)
-    #validate_message(reply, 'execute_reply', msg_id)
-    busy = kc.get_iopub_msg(timeout=10)
-    #validate_message(busy, 'status', msg_id)
-    #nt.assert_equal(busy['content']['execution_state'], 'busy')
-
-    if not kwargs.get('silent'):
-        execute_input = kc.get_iopub_msg(timeout=10)
-        # validate_message(execute_input, 'execute_input', msg_id)
-        #nt.assert_equal(execute_input['content']['code'], code)
-
-    return msg_id, reply['content']
-
-
 class TestKernel(unittest.TestCase):
-    def testDict(self):
-        with kernel() as kc:
+    def testInterpolation(self):
+        with sos_kernel() as kc:
             iopub = kc.iopub_channel
-            msg_id, content = execute(kc=kc, code="print ('hi')")
-            #stdout, stderr = assemble_output(iopub)
-            #nt.assert_equal(stdout, 'hi\n')
-            #nt.assert_equal(stderr, '')
-            #_check_master(kc, expected=True)
+            msg_id, content = execute(kc=kc, code="print('a=${100+11}')")
+            stdout, stderr = assemble_output(iopub)
+            nt.assert_equal(stdout, 'a=111\n')
+            nt.assert_equal(stderr, '')
+
+
 
 if __name__ == '__main__':
     unittest.main()
