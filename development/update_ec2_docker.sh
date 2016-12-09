9710f96a00fa08b51734cf3a9abd35f076e0a9f8
Bo Peng <ben.bog@gmail.com>
2016-12-08 20:01:08 -0600

    Update our public server with the latest docker image

--- development/update_ec2_docker.sh
+++ development/update_ec2_docker.sh
@@ -4,9 +4,7 @@ docker rm $(docker stop $(docker ps -a -q --filter="name=tmp"))
 docker stop proxy
 docker rm proxy
 
-cd SOS
-git pull
-docker build -t mdabioinfo/sos-notebook:latest -f /home/ubuntu/SOS/development/docker-notebook/Dockerfile .
+docker pull mdabioinfo/sos-notebook:latest
 
 export TOKEN=$( head -c 30 /dev/urandom | xxd -p )
 docker run --net=host -d -e CONFIGPROXY_AUTH_TOKEN=$TOKEN --name=proxy jupyter/configurable-http-proxy --default-target http://127.0.0.1:9999
