dce2227e1f6a8ecc5946171f75954c5838610d25
Bo Peng <ben.bog@gmail.com>
2017-01-28 08:29:27 -0600

    Move the language selection dropdown of the panel cell to the top to avoid overlapping with input.

--- sos/jupyter/js/sos_panel.js
+++ sos/jupyter/js/sos_panel.js
@@ -54,6 +54,8 @@ var panel = function(nb) {
     // remove input and output prompt to save some space
     var ip = this.cell.element[0].getElementsByClassName('input_prompt');
     ip[0].parentNode.removeChild(ip[0]);
+    // move the language selection stuff to the top
+    this.cell.element[0].getElementsByClassName('celltoolbar')[0].style.marginBottom = 0;
 
     // override ctrl/shift-enter to execute me if I'm focused instead of the notebook's cell
     var execute_and_select_action = this.km.actions.register({
