fed9a4ef9bfc64027d8ff467c589131475eaae32
Bo Peng <ben.bog@gmail.com>
2017-03-10 16:31:31 -0600

    Test sos actions in juypter

---
+++ sos/jupyter/test/test_action.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+#
+# This file is part of Script of Scripts (SoS), a workflow system
+# for the execution of commands and scripts in different languages.
+# Please visit https://github.com/vatlab/SOS for more information.
+#
+# Copyright (C) 2016 Bo Peng (bpeng@mdanderson.org)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+#
+# NOTE: for some namespace reason, this test can only be tested using
+# nose. 
+#
+# % nosetests test_kernel.py
+#
+#
+import os
+import unittest
+from ipykernel.tests.utils import execute, wait_for_idle
+from sos.jupyter.test.utils import sos_kernel, get_result
+
+class TestAction(unittest.TestCase):
+    #
+    # Beacuse these tests would be called from sos/test, we
+    # should switch to this directory so that some location
+    # dependent tests could run successfully
+    #
+    def setUp(self):
+        self.olddir = os.getcwd()
+        if os.path.dirname(__file__):
+            os.chdir(os.path.dirname(__file__))
+
+    def tearDown(self):
+        os.chdir(self.olddir)
+
+    def testInterpolation(self):
+        with sos_kernel() as kc:
+            iopub = kc.iopub_channel
+            msg_id, content = execute(kc=kc, code='''
+[a]
+b=10
+
+[default]
+sos_run('a')
+''')
+            wait_for_idle(kc)
+            msg_id, content = execute(kc=kc, code="b")
+            res = get_result(iopub)
+            self.assertEqual(res, '10')
+
+
+if __name__ == '__main__':
+    unittest.main()
