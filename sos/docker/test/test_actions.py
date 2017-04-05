b42356f03b08ee0a1e728a69655dded796c151b9
Bo Peng <ben.bog@gmail.com>
2017-04-05 15:19:52 -0500

    Simplify a test

--- sos/docker/test/test_actions.py
+++ sos/docker/test/test_actions.py
@@ -245,8 +245,6 @@ FROM ubuntu:latest
 MAINTAINER Andrew Odewahn "odewahn@oreilly.com"
 
 RUN apt-get update
-RUN apt-get install -y python python-pip wget
-RUN pip install Flask
 
 WORKDIR /home
 ''')
