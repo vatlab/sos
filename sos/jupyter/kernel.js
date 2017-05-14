cc628aa926e50502560279830e78fbe8d73b7f03
Bo Peng <ben.bog@gmail.com>
2017-05-14 12:47:33 -0500

    Implement tasks, preview --workflow, and help buttons #606

--- sos/jupyter/kernel.js
+++ sos/jupyter/kernel.js
@@ -946,42 +946,58 @@ define([
         this.cell.element.hide();
 
         // move input prompt on top of the cell
-		var panel_buttons = $('<div class="toc_buttons"/>');
+        var panel_buttons = $('<div class="toc_buttons"/>');
         panel_buttons.append(
-            $("<span/>")
-            .html("&nbsp;&nbsp;&nbsp;&nbsp;")
-		).append(
-			$("<i class='fa  fa-chevron-left'></i>")
-        ).append(
-            $("<span/>")
-            .html("&nbsp;&nbsp")
-		).append(
-			$("<i class='fa  fa-chevron-right'></i>")
-        ).append(
-            $("<span/>")
-            .html("&nbsp;&nbsp")
-		).append(
             $("<a/>").attr('href', '#')
+            .append($("<i class='fa  fa-list-ul'></i>"))
             .click(function() {
-				console.log('clicked');
-				show_toc();
-				return false;
-			}).append($("<i class='fa  fa-list-ul'></i>"))
+                show_toc();
+                return false;
+            })
         ).append(
             $("<span/>")
             .html("&nbsp;&nbsp")
-		).append(
-			$("<a/>").attr('href', '#').append(
-			$("<i class='fa  fa-tasks'></i>"))
+        ).append(
+            $("<a/>").attr('href', '#')
+            .append($("<i class='fa  fa-tasks'></i>"))
             .click(function() {
-				show_toc();
-				return false;
-			})
+                var panel_cell = window.my_panel.cell;
+                panel_cell.clear_input();
+                panel_cell.set_text('%tasks');
+                panel_cell.clear_output();
+                panel_cell.execute();
+                return false;
+            })
+        ).append(
+            $("<span/>")
+            .html("&nbsp;&nbsp")
+        ).append(
+            $("<a/>").attr('href', '#')
+            .append($("<i class='fa fa-file-text'></i>"))
+            .click(function() {
+                var panel_cell = window.my_panel.cell;
+                panel_cell.clear_input();
+                panel_cell.set_text('%preview --workflow');
+                panel_cell.clear_output();
+                panel_cell.execute();
+                return false;
+            })
+        ).append(
+            $("<span/>")
+            .html("&nbsp;&nbsp")
+        ).append(
+            $("<a/>").attr('href', '#')
+            .append($("<i class='fa fa-book'></i>"))
+            .click(function() {
+                var win = window.open('http://vatlab.github.io/SOS/doc/documentation/Notebook_Interface.html', '_blank');
+                win.focus();
+                return false;
+            })
         )
 
         var prot = this.cell.element.find('div.input_prompt');
         prot.css('min-width', '0ex').css('width', '0ex').text('In [-]:');
-		prot.parent().append(panel_buttons);
+        prot.parent().append(panel_buttons);
 
         // move the language selection stuff to the top
         this.cell.element[0].getElementsByClassName('celltoolbar')[0].style.marginBottom = 0;
@@ -1298,14 +1314,14 @@ define([
                 '    counter-increment: item;' +
                 '    content: counters(item, ".")" ";' +
                 '}' +
-				'.toc_buttons {' +
-				'    font-size: 80%;' +
-				'    color: gray;' +
-				'    position: fixed;' +
-				'    padding-left: 40pt;' +
-				'    padding-top: 0.7em;' +
-				'    z-index: 1000;' +
-				'}' +
+                '.toc_buttons {' +
+                '    font-size: 80%;' +
+                '    color: gray;' +
+                '    position: fixed;' +
+                '    padding-left: 50pt;' +
+                '    padding-top: 0.7em;' +
+                '    z-index: 1000;' +
+                '}' +
                 '.lev1 {margin-left: 5px}' +
                 '.lev2 {margin-left: 10px}' +
                 '.lev3 {margin-left: 10px}' +
