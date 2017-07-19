93f4d0776aadf928cc76f04ed810c4e7b44a1d97
U-MDANDERSON\BPeng1 <BPeng1@BCBW31517465254.mdanderson.edu>
2017-07-19 13:31:03 -0500

    Roughly working %put magic for Matlab #394

--- src/sos/Matlab/kernel.py
+++ src/sos/Matlab/kernel.py
@@ -21,6 +21,7 @@
 #
 import pandas as pd
 
+import os
 from collections import Sequence
 import tempfile
 from sos.utils import short_repr, env
@@ -127,111 +128,9 @@ def _Matlab_repr(obj):
 # data.frame    n > 0    DataFrame
 
 Matlab_init_statements = r'''
-__py_repr_logical_1 <- function(obj) {
-    if(obj)
-        'True'
-    else
-        'False'
-}
-__py_repr_integer_1 <- function(obj) {
-    as_character(obj)
-}
-__py_repr_double_1 <- function(obj) {
-    as_character(obj)
-}
-__py_repr_character_1 <- function(obj) {
-    paste0('r"""', obj, '"""')
-}
-__has_row_names <- function(df) {
-  !all(row_names(df)==seq(1, nrow(df)))
-}
-__py_repr_dataframe <- function(obj) {
-    if (!require("mat")) {
-        install_packages('mat', repos='http://cran_stat_ucla_edu/')
-        }
-    library(mat)
-    tf = tempfile('mat')
-    write_mat(obj, tf)
-    if (__has_row_names(obj)) {
-        paste0("read_dataframe(r'", tf, "')_set_index([", __py_repr(row_names(obj)),"])")
-    } else {
-        paste0("read_dataframe(r'", tf, "')")
-    }
-}
-__py_repr_matrix <- function(obj) {
-    if (!require("mat")) {
-        install_packages('mat', repos='http://cran_stat_ucla_edu/')
-        }
-    library(mat)
-    tf = tempfile('mat')
-    write_mat(as_data_frame(obj), tf)
-    if (__has_row_names(obj)) {
-       paste0("read_dataframe(r'", tf, "')_set_index([", __py_repr(row_names(obj)),"])_as_matrix()")
-    } else {
-       paste0("read_dataframe(r'", tf, "')_as_matrix()")
-    }
-}
-__py_repr_n <- function(obj) {
-    paste("[",
-        paste(sapply(obj, __py_repr), collapse=','),
-        "]")
-}
-__py_repr <- function(obj) {
-    if (is_matrix(obj)) {
-        __py_repr_matrix(obj)
-    } else if (is_data_frame(obj)) {
-        __py_repr_dataframe(obj)
-    } else if (is_null(obj)) {
-        'None'
-    } else if (is_integer(obj)) {
-        if (length(obj) == 1)
-            __py_repr_integer_1(obj)
-        else
-            paste("[", paste(obj, collapse=','), "]")
-    } else if (is_double(obj)){
-        if (length(obj) == 1)
-            __py_repr_double_1(obj)
-        else
-            paste("[", paste(obj, collapse=','), "]")
-    } else if (is_character(obj)) {
-        if (length(obj) == 1)
-            __py_repr_character_1(obj)
-        else {
-            paste("[", paste(sapply(obj, __py_repr_character_1), collapse=','), "]")
-        }
-    } else if (is_logical(obj)) {
-        if (length(obj) == 1)
-            __py_repr_logical_1(obj)
-        else
-            __py_repr_n(obj)
-    } else if (is_list(obj)) {
-        # if the list has no name
-        if (is_null(names(obj)))
-            __py_repr_n(obj)
-        else {
-            paste("{",
-                  paste(sapply(names(obj), function (x)
-                      paste(shQuote(gsub("\\_", "_", as_character(x))), ":", __py_repr(obj[[x]]))),
-                      collapse=','),
-                  "}")
-        }
-    } else {
-        "'Untransferrable variable'"
-    }
-}
-__read_mat <- function(filename, index=NULL) {
-    if (! suppressMessages(suppressWarnings(require("mat", quietly = TRUE)))) {
-        try(install_packages('mat', repos='http://cran_stat_ucla_edu/'), silent=TRUE)
-        if (!suppressMessages(suppressWarnings(require("mat"))))
-            stop('Failed to install mat library')
-    }
-    suppressPackageStartupMessages(library(mat, quietly = TRUE))
-    data = as_data_frame(read_mat(filename))
-    if (!is_null(index))
-        rownames(data) <- index
-    return(data)
-}
-'''
+path(path, {!r})
+'''.format(os.path.split(__file__)[0])
+print(Matlab_init_statements)
 
 
 class sos_Matlab:
@@ -242,45 +141,42 @@ class sos_Matlab:
         self.init_statements = Matlab_init_statements
 
     def get_vars(self, names):
-        for name in names:
-            if name.startswith('_'):
-                self.sos_kernel.warn('Variable {} is passed from SoS to kernel {} as {}'.format(name, self.kernel_name, '.' + name[1:]))
-                newname = '.' + name[1:]
-            else:
-                newname = name
-            matlab_repr = _Matlab_repr(env.sos_dict[name])
-            self.sos_kernel.run_cell('{} = {}'.format(newname, matlab_repr), True, False, on_error='Failed to get variable {} to R'.format(name))
+        #for name in names:
+        #    if name.startswith('_'):
+        #        self.sos_kernel.warn('Variable {} is passed from SoS to kernel {} as {}'.format(name, self.kernel_name, '.' + name[1:]))
+        #        newname = '.' + name[1:]
+        #    else:
+        #        newname = name
+        #    matlab_repr = _Matlab_repr(env.sos_dict[name])
+        #    self.sos_kernel.run_cell('{} = {}'.format(newname, matlab_repr), True, False, on_error='Failed to get variable {} to R'.format(name))
+        pass
 
     def put_vars(self, items, to_kernel=None):
         # first let us get all variables with names starting with sos
-        response = self.sos_kernel.get_response('cat(..py.repr(ls()))', ('stream',), name=('stdout',))[0][1]
-        all_vars = eval(response['text'])
-        all_vars = [all_vars] if isinstance(all_vars, str) else all_vars
-
-        items += [x for x in all_vars if x.startswith('sos')]
-
-        for item in items:
-            if '.' in item:
-                self.sos_kernel.warn('Variable {} is put to SoS as {}'.format(item, item.replace('.', '_')))
+        response = self.sos_kernel.get_response("display(cell2mat(who('sos*')))", ('stream',), name=('stdout',), debug=True)[0][1]
+        all_vars = response['text'].strip().split()
+        # in case there is no var with name starts with sos, the response would be
+        #      []\n\n\n
+        items += [x for x in all_vars if x != '[]']
 
         if not items:
             return {}
 
-        py_repr = 'cat(..py.repr(list({})))'.format(','.join('{0}={0}'.format(x) for x in items))
-        response = self.sos_kernel.get_response(py_repr, ('stream',), name=('stdout',))[0][1]
-        expr = response['text']
+        result = {}
+        for item in items:
+            py_repr = 'display(sos_py_repr({}))'.format(item)
+            response = self.sos_kernel.get_response(py_repr, ('stream',), name=('stdout',), debug=True)[0][1]
+            expr = response['text']
 
-        try:
-            if 'save_mat' in expr:
-                # imported to be used by eval
-                from scipy.io import read_mat
-                # suppress flakes warning
-                read_mat
-            # evaluate as raw string to correctly handle \\ etc
-            return eval(expr)
-        except Exception as e:
-            self.sos_kernel.warn('Failed to evaluate {!r}: {}'.format(expr, e))
-            return None
+            try:
+                if 'loadmat' in expr:
+                    # imported to be used by eval
+                    from scipy.io import loadmat
+                # evaluate as raw string to correctly handle \\ etc
+                result[item] = eval(expr)
+            except Exception as e:
+                self.sos_kernel.warn('Failed to evaluate {!r}: {}'.format(expr, e))
+        return result
 
     def sessioninfo(self):
-        return self.sos_kernel.get_response(r'cat(paste(capture.output(sessionInfo()), collapse="\n"))', ('stream',), name=('stdout',))[0]
+        return self.sos_kernel.get_response(r'ver', ('stream',), name=('stdout',))[0][1]['text']
