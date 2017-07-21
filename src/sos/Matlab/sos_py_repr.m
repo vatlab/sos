ab76c957f9c781c781ed664efcd7d70da01a7816
Man Chong Leong <ml69@rice.edu>
2017-07-21 14:32:41 -0500

    Matlab->SoS data transfer #394

--- src/sos/Matlab/sos_py_repr.m
+++ src/sos/Matlab/sos_py_repr.m
@@ -18,18 +18,66 @@
 % along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 function [repr] = sos_py_repr (obj)
-    if isfloat(obj)
-        repr = num2str(obj);
+    if isnumeric(obj)
+
+        if isinf(obj)
+            if obj>0
+                repr='np.inf';
+            else
+                repr='-np.inf';
+            end
+
+        elseif isreal(obj)==0
+            rl=num2str(real(obj));
+            im=num2str(imag(obj));
+            repr=strcat('complex(',rl,',',im,')');
+
+        elseif isnan(obj)
+            repr='None';
+
+        elseif isvector(obj)
+            if length(obj)==1
+                repr=num2str(obj);
+
+            else
+                nu=sprintf('%.0f,' ,obj);
+                nu=nu(1:end-1);
+                repr=strcat('np.array([',nu,'])');
+            end
+
+        elseif ismatrix(obj)
+            save(fullfile(tempdir,'mat2py.mat'),'obj');
+            %dirpath=tempdir;
+            %repr="mat_contents = sio.loadmat('/Users/manchongleong/Desktop/mat2py.mat')"
+            repr=char(strcat("sio.loadmat('",tempdir,"mat2py.mat')","['","obj","']"));
+
+        else
+            repr=num2str(obj);
+        end
+
+    elseif isstruct(obj)
+        save(fullfile(tempdir,'stru2py.mat'),'obj');
+        dirpath=tempdir;
+        %repr="stru2py = sio.loadmat('/Users/manchongleong/Desktop/stru2py.mat')";
+        repr=strcat("sio.loadmat('",tempdir,"stru2py.mat')","['","obj","']");
+
+    elseif iscell(obj)
+        save(fullfile(tempdir,'cell2py.mat'),'obj');
+        dirpath=tempdir;
+        %repr="stru2py = sio.loadmat('/Users/manchongleong/Desktop/stru2py.mat')";
+        repr=strcat("sio.loadmat('",tempdir,"cell2py.mat')","['","obj","']");
+
     elseif islogical(obj)
         if obj
             repr = 'True';
         else
             repr = 'False';
-		end
+        end
+
     else
         % unrecognized/unsupported datatype is transferred from
         % matlab to Python as None
         repr = 'None';
-	end
+    end
 end
 
