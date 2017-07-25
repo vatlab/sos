97cad5dd01ec0b70a548d88130419096c44b585d
Man Chong Leong <ml69@rice.edu>
2017-07-25 15:53:40 -0500

    fixed the problem of string with double quote--raw sting #394

--- src/sos/Matlab/sos_py_repr.m
+++ src/sos/Matlab/sos_py_repr.m
@@ -56,7 +56,7 @@ if isnumeric(obj)
     end
 % string
 elseif isstr(obj)
-    repr =strcat('"""',obj,'"""');
+    repr =strcat('r"""',obj,'"""');
 % structure
 elseif isstruct(obj)
     save(fullfile(tempdir, 'stru2py.mat'), 'obj');
