1cf5fc0057bbd7421c917365e98eec5d3e375015
Bo Peng <ben.bog@gmail.com>
2018-02-13 20:16:41 -0600

    Improve waiting message to add specific lock file #871

--- src/sos/dag.py
+++ src/sos/dag.py
@@ -193,8 +193,7 @@ class SoS_DAG(nx.DiGraph):
         pending_jobs = [x for x in self.nodes() if x._status == 'signature_pending']
         if pending_jobs:
             try:
-                notifier = ActivityNotifier('Waiting for another process for output {}'.format(short_repr(
-                    sum([node._signature[0] for node in pending_jobs], []))))
+                notifier = ActivityNotifier(f'Waiting for {len(pending_jobs)} pending job{"s: e.g." if len(pending_jobs) > 1 else ":"} output {pending_jobs[0]._signature[0]} with signature file {pending_jobs[0]._signature[1] + "_"}')
                 while True:
                     for node in pending_jobs:
                         # if it has not been executed
--- src/sos/utils.py
+++ src/sos/utils.py
@@ -769,9 +769,10 @@ class ActivityNotifier(threading.Thread):
                     prog.close()
                 break
             if not prog:
-                prog = ProgressBar(desc=self.msg, position=0, bar_format='{desc}', total=100000000)
+                print(self.msg)
+                prog = ProgressBar(desc='', position=0, bar_format='{desc}', total=100000000)
             second_elapsed = time.time() - self.start_time
-            prog.set_description(self.msg + ' ({}{})'.format(
+            prog.set_description(' ... {}{}'.format(
                     '' if second_elapsed < 86400 else f'{int(second_elapsed/86400)} day{"s" if second_elapsed > 172800 else ""} ',
                     time.strftime('%H:%M:%S', time.gmtime(second_elapsed)) ))
             prog.update(1)
