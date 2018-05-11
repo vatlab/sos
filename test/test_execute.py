c3fac09cc0624874c04df4c336961c3d941bd4eb
Bo Peng <ben.bog@gmail.com>
2018-05-10 19:34:21 -0500

    Fix test for -q -r without value

--- test/test_execute.py
+++ test/test_execute.py
@@ -75,19 +75,6 @@ a =1
                                          stdout=subprocess.DEVNULL, shell=True), 0)
         self.assertEqual(subprocess.call('sos run -h', stderr=subprocess.DEVNULL,
                                          stdout=subprocess.DEVNULL, shell=True), 0)
-        # list queues
-        self.assertEqual(subprocess.call('sos run test_cl -q',
-                                         stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL, shell=True), 0)
-        self.assertEqual(subprocess.call('sos execute a23 -q', shell=True), 0)
-        #self.assertEqual(subprocess.call('sos resume -r', stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL, shell=True), 0)
-        self.assertEqual(subprocess.call('sos push something -t',
-                                         stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL, shell=True), 0)
-        self.assertEqual(subprocess.call('sos pull something -f',
-                                         stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL, shell=True), 0)
-        self.assertEqual(subprocess.call('sos preview something -r',
-                                         stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL, shell=True), 0)
-        self.assertEqual(subprocess.call('sos kill -a -q', stderr=subprocess.DEVNULL,
-                                         stdout=subprocess.DEVNULL, shell=True), 0)
         #
         self.assertEqual(subprocess.call('sos run test_cl -w -W',
                                          stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL, shell=True), 1)
