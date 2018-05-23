312ccccd2174642ee0877d0763f6934a9b53242c
Bo <BoPeng@users.noreply.github.com>
2018-05-23 15:41:23 -0500

    Update license in setup.py

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ setup(name="sos",
       author_email='bpeng@mdanderson.org',
       maintainer='Bo Peng',
       maintainer_email='bpeng@mdanderson.org',
-      license='GPL3',
+      license='3-clause BSD',
       include_package_data=True,
       classifiers=[
           'Development Status :: 4 - Beta',
