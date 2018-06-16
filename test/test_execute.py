669600b3130a6fb2293f4c001cf1f35868ac608b
Bo Peng <ben.bog@gmail.com>
2018-06-16 11:05:16 -0500

    Add a test for single-step workflow triggered by auxiliary step

--- test/test_execute.py
+++ test/test_execute.py
@@ -1233,7 +1233,32 @@ depends: "a_2"
         with open('a_1') as a1, open('a_2') as a2:
             self.assertEqual(a1.read(), a2.read())
 
+    def testDependsAuxiAndSingleStepForward(self):
+        '''Test depends on auxiliary, which then depends on a single-step forward-workflow'''
+        file_target('a_1').remove('both')
+        file_target('a_2').remove('both')
+        script = SoS_Script('''
 
+[hg_1]
+output: 'a_1'
+sh:
+  echo "something" > a_1
+
+[star: provides = "a_2"]
+depends: sos_step('hg')
+sh:
+  cp  a_1 a_2
+
+[default]
+depends: "a_2"
+        ''')
+        wf = script.workflow()
+        res = Base_Executor(wf).run()
+        self.assertEqual(res['__completed__']['__step_completed__'], 4)
+        self.assertTrue(os.path.isfile('a_1'))
+        self.assertTrue(os.path.isfile('a_2'))
+        with open('a_1') as a1, open('a_2') as a2:
+            self.assertEqual(a1.read(), a2.read())
         
 if __name__ == '__main__':
     unittest.main()
