7ecd657a22dcf97c5115e32468bf2b331464f573
gaow <wangow@gmail.com>
2018-09-05 13:45:49 -0500

    Update parser.py

--- src/sos/parser.py
+++ src/sos/parser.py
@@ -22,7 +22,7 @@ from .eval import on_demand_options
 from .syntax import (INDENTED, SOS_AS, SOS_CELL, SOS_DIRECTIVE, SOS_DIRECTIVES,
                      SOS_ELIF, SOS_ELSE, SOS_ENDIF, SOS_FORMAT_LINE,
                      SOS_FORMAT_VERSION, SOS_FROM_INCLUDE, SOS_IF, SOS_INCLUDE,
-                     SOS_MAGIC, SOS_SECTION_HEADER, SOS_SECTION_NAME,so
+                     SOS_MAGIC, SOS_SECTION_HEADER, SOS_SECTION_NAME,
                      SOS_SECTION_OPTION, SOS_STRU, SOS_SUBWORKFLOW, SOS_ACTION_OPTIONS)
 from .targets import file_target, path, paths, sos_targets, textMD5
 from .utils import Error, env, locate_script, text_repr, format_par
