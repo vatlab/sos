cd829e567e9c308c2a0c41ca8818c228565281a6
Bo Peng <ben.bog@gmail.com>
2018-11-21 11:06:12 -0600

    Show error message instead of output for status checking #1092

--- src/sos/task_engines.py
+++ src/sos/task_engines.py
@@ -373,7 +373,7 @@ class TaskEngine(threading.Thread):
         except subprocess.CalledProcessError as e:
             if verbosity >= 3:
                 env.logger.warning(
-                    f'Failed to query status of tasks on {self.alias}: {e.output}')
+                    f'Failed to query status of tasks on {self.alias}: {e.stderr.decode()}')
             return ''
 
     def kill_tasks(self, tasks, tags=None, all_tasks=False):
