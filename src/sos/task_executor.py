8f44421c7da16eedcaaf32c73e8ade7abdad66e6
Bo Peng <ben.bog@gmail.com>
2019-09-30 11:38:38 -0500

    Add a comment on #1304

--- src/sos/task_executor.py
+++ src/sos/task_executor.py
@@ -386,6 +386,13 @@ class BaseTaskExecutor(object):
             open(self.master_stderr, 'w').close()
 
         # options specified from command line, most likely from cluster system
+        #
+        # As explained in ticket 1304:
+        #
+        # From the point of cluster system, this is only one "job" on each
+        # node. The "n_procs" is the "number of cpus per job per node", which,
+        # is "cores" (per task) * "n_workers" (per node). Therefore,
+        # "n_procs" should not be used.
         n_nodes, n_procs = self._parse_num_workers(env.config['worker_procs'])
 
         # regular trunk_workers = ?? (0 was used as default)
