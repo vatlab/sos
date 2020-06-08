49636f224ca8399d43b290b76f3f434e91509746
Joann Feng <joannfeng@utexas.edu>
2020-06-08 13:32:51 -0500

    Reorganize test_bash_actions.py

--- test/test_bash_actions.py
+++ test/test_bash_actions.py
@@ -5,7 +5,9 @@
 import os
 import shutil
 import unittest
+import pytest
 
+from sos import execute_workflow
 from sos.parser import SoS_Script
 from sos.targets import file_target
 from sos.utils import env
@@ -35,90 +37,69 @@ class TestActions(unittest.TestCase):
 
     def test_bash(self):
         '''Test action bash'''
-        script = SoS_Script(r'''
-[0]
-bash:
-echo 'Echo'
-''')
-        wf = script.workflow()
-        Base_Executor(wf).run()
-        script = SoS_Script(r'''
-[0]
-bash:
-echo 'Echo
-''')
-        wf = script.workflow()
-        self.assertRaises(Exception, Base_Executor(wf).run)
+        script = execute_workflow(r'''
+            [0]
+            bash:
+                echo 'Echo'
+        ''')
+        with pytest.raises(Exception):
+            execute_workflow(script)
 
     def test_sh(self):
         '''Test action run'''
-        script = SoS_Script(r'''
-[0]
-sh:
-echo 'Echo'
-''')
-        wf = script.workflow()
-        Base_Executor(wf).run()
-        script = SoS_Script(r'''
-[0]
-sh:
-echo 'Echo
-''')
-        wf = script.workflow()
-        self.assertRaises(Exception, Base_Executor(wf).run)
+        script = execute_workflow(r'''
+            [0]
+            sh:
+                echo 'Echo'
+        ''')
+        with pytest.raises(Exception):
+            execute_workflow(script)
 
     def test_csh(self):
         '''Test action csh'''
         if not shutil.which('csh'):
             return
-        script = SoS_Script(r'''
-[0]
-csh:
-     foreach color (red orange yellow green blue)
-        echo $color
-     end
-''')
-        wf = script.workflow()
-        Base_Executor(wf).run()
+        script = execute_workflow(r'''
+            [0]
+            csh:
+                foreach color (red orange yellow green blue)
+                    echo $color
+                end
+        ''')
 
     def test_tcsh(self):
         '''Test action tcsh'''
         if not shutil.which('tcsh'):
             return
-        script = SoS_Script(r'''
-[0]
-tcsh:
-     foreach color (red orange yellow green blue)
-        echo $color
-     end
-''')
-        wf = script.workflow()
-        Base_Executor(wf).run()
+        script = execute_workflow(r'''
+            [0]
+            tcsh:
+                foreach color (red orange yellow green blue)
+                    echo $color
+                end
+        ''')
 
     def test_zsh(self):
         '''Test action zsh'''
         if not shutil.which('zsh'):
             return
-        script = SoS_Script(r'''
-[0]
-zsh:
-echo "Hello World!", $SHELL
-''')
-        wf = script.workflow()
-        Base_Executor(wf).run()
+        script = execute_workflow(r'''
+            [0]
+            zsh:
+                echo "Hello World!", $SHELL
+        ''')
 
     def test_args(self):
         '''Test args option of scripts'''
         if os.path.isfile('a.txt'):
             file_target('a.txt').unlink()
-        script = SoS_Script(r'''
-[0]
-sh: args='-n {filename:q}'
-    touch a.txt
-''')
-        wf = script.workflow()
-        Base_Executor(wf).run()
-        self.assertFalse(os.path.exists('a.txt'))
+        script = execute_workflow(r'''
+            [0]
+            sh: args='-n {filename:q}'
+                touch a.txt
+        ''')
+
+        assert not os.path.exists('a.txt')
 
 
 if __name__ == '__main__':
