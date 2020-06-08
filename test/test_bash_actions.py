c17afb3803b8b2b041aefb6925e2be0820aaea97
Joann Feng <joannfeng@utexas.edu>
2020-06-08 14:53:37 -0500

    Reorganize test_bash_actions.py 2

--- test/test_bash_actions.py
+++ test/test_bash_actions.py
@@ -35,72 +35,67 @@ class TestActions(unittest.TestCase):
         #
         self.temp_files.extend(files)
 
-    def test_bash(self):
-        '''Test action bash'''
-        script = execute_workflow(r'''
-            [0]
-            bash:
-                echo 'Echo'
-        ''')
-        with pytest.raises(Exception):
-            execute_workflow(script)
+def test_bash():
+    '''Test action bash'''
+    script = execute_workflow(r'''
+        [0]
+        bash:
+            echo 'Echo'
+    ''')
+    with pytest.raises(Exception):
+        execute_workflow(script)
 
-    def test_sh(self):
-        '''Test action run'''
-        script = execute_workflow(r'''
-            [0]
-            sh:
-                echo 'Echo'
-        ''')
-        with pytest.raises(Exception):
-            execute_workflow(script)
+def test_sh():
+    '''Test action run'''
+    script = execute_workflow(r'''
+        [0]
+        sh:
+            echo 'Echo'
+    ''')
+    with pytest.raises(Exception):
+        execute_workflow(script)
 
-    def test_csh(self):
-        '''Test action csh'''
-        if not shutil.which('csh'):
-            return
-        script = execute_workflow(r'''
-            [0]
-            csh:
-                foreach color (red orange yellow green blue)
-                    echo $color
-                end
-        ''')
+def test_csh():
+    '''Test action csh'''
+    if not shutil.which('csh'):
+        return
+    script = execute_workflow(r'''
+        [0]
+        csh:
+            foreach color (red orange yellow green blue)
+                echo $color
+            end
+    ''')
 
-    def test_tcsh(self):
-        '''Test action tcsh'''
-        if not shutil.which('tcsh'):
-            return
-        script = execute_workflow(r'''
-            [0]
-            tcsh:
-                foreach color (red orange yellow green blue)
-                    echo $color
-                end
-        ''')
+def test_tcsh():
+    '''Test action tcsh'''
+    if not shutil.which('tcsh'):
+        return
+    script = execute_workflow(r'''
+        [0]
+        tcsh:
+            foreach color (red orange yellow green blue)
+                echo $color
+            end
+    ''')
 
-    def test_zsh(self):
-        '''Test action zsh'''
-        if not shutil.which('zsh'):
-            return
-        script = execute_workflow(r'''
-            [0]
-            zsh:
-                echo "Hello World!", $SHELL
-        ''')
+def test_zsh():
+    '''Test action zsh'''
+    if not shutil.which('zsh'):
+        return
+    script = execute_workflow(r'''
+        [0]
+        zsh:
+            echo "Hello World!", $SHELL
+    ''')
 
-    def test_args(self):
-        '''Test args option of scripts'''
-        if os.path.isfile('a.txt'):
-            file_target('a.txt').unlink()
-        script = execute_workflow(r'''
-            [0]
-            sh: args='-n {filename:q}'
-                touch a.txt
-        ''')
-
-        assert not os.path.exists('a.txt')
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_args():
+    '''Test args option of scripts'''
+    if os.path.isfile('a.txt'):
+        file_target('a.txt').unlink()
+    script = execute_workflow(r'''
+        [0]
+        sh: args='-n {filename:q}'
+            touch a.txt
+    ''')
+    assert not os.path.exists('a.txt')
