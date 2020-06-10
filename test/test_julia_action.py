dc763ce49143ad775b8de7c8d82d14d9c246613d
Joann Feng <joannfeng@utexas.edu>
2020-06-10 12:01:49 -0500

    Reorganize test_julia_action.py and test_matlab_action.py

--- test/test_julia_action.py
+++ test/test_julia_action.py
@@ -5,40 +5,22 @@
 
 import os
 import shutil
-import unittest
 
-from sos.parser import SoS_Script
-from sos.targets import file_target
-from sos.utils import env
-from sos.workflow_executor import Base_Executor
+import pytest
+
+from sos import execute_workflow
 
 
-class TestActions(unittest.TestCase):
-
-    def setUp(self):
-        env.reset()
-        self.temp_files = []
-
-    def tearDown(self):
-        for f in self.temp_files:
-            file_target(f).unlink()
-
-    @unittest.skipIf(not shutil.which('julia'), "julia not available")
-    def test_julia(self):
-        '''Test action Julia'''
-        if os.path.isfile('julia_example.txt'):
-            os.remove('julia_example.txt')
-        script = SoS_Script(r'''
-[0]
-julia:
-    open("julia_example.txt", "w") do f
-        write(f, "A, B, C, D\n")
-     end
-''')
-        wf = script.workflow()
-        Base_Executor(wf).run()
-        self.assertTrue(os.path.isfile('julia_example.txt'))
-
-
-if __name__ == '__main__':
-    unittest.main()
+@pytest.mark.skipif(not shutil.which('julia'), reason="julia not available")
+def test_julia(self):
+    '''Test action Julia'''
+    if os.path.isfile('julia_example.txt'):
+        os.remove('julia_example.txt')
+    execute_workflow(r'''
+        [0]
+        julia:
+            open("julia_example.txt", "w") do f
+                write(f, "A, B, C, D\n")
+            end
+    ''')
+    assert os.path.isfile('julia_example.txt')
