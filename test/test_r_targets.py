e3f6d7f840268b121054e35321c6f5da0c2496b3
Bo Peng <ben.bog@gmail.com>
2020-11-27 23:02:21 -0600

    Update test_r_targets

--- test/test_r_targets.py
+++ test/test_r_targets.py
@@ -6,69 +6,54 @@
 import os
 import shutil
 import subprocess
-import unittest
-
-from sos.parser import SoS_Script
-from sos.targets import file_target
-from sos.utils import env
-from sos.workflow_executor import Base_Executor
+import pytest
+from sos import execute_workflow
 
 
-class TestTarget(unittest.TestCase):
-
-    def setUp(self):
-        env.reset()
-        subprocess.call('sos remove -s', shell=True)
-        self.temp_files = []
-
-    def tearDown(self):
-        for f in self.temp_files:
-            file_target(f).unlink()
-
-    @unittest.skipIf(not shutil.which('Rscript') or 'TRAVIS' in os.environ, 'R not installed or cannot auto-install')
-    def test_r_library(self):
-        '''Test target R_Library'''
-        script = SoS_Script('''
-[default]
-depends: R_library("dplyr", autoinstall=True)
-R:
-    library('dplyr')
-''')
-        wf = script.workflow()
-        Base_Executor(wf).run()
-
-    @unittest.skipIf(not shutil.which('Rscript'), 'R not installed')
-    def test_depends_r_library(self):
-        '''Testing depending on R_library'''
-        # first remove xtable package
-        subprocess.call('R CMD REMOVE xtable', shell=True)
-        script = SoS_Script('''
-[0]
-
-depends: R_library('xtable', autoinstall=True)
-R:
-    library('xtable')
-    ## Demonstrate data.frame
-    tli.table <- xtable(cars)
-''')
-        wf = script.workflow()
-        Base_Executor(wf).run()
-
-    @unittest.skipIf(not shutil.which('Rscript'), 'R not installed')
-    def test_reexecution(self):
-        '''Test re-execution of steps with R_library'''
-        script = SoS_Script('''
-[1]
-depends: R_library("bmass>=1.0.3", autoinstall=True)
-output: '1.txt'
-run: expand=True
-    sleep 5
-    touch {_output}
-''')
-        wf = script.workflow()
-        Base_Executor(wf).run()
-        Base_Executor(wf).run()
+@pytest.mark.skipif(
+    not shutil.which("Rscript") or "TRAVIS" in os.environ, reason="R not installed"
+)
+def test_r_library():
+    """Test target R_Library"""
+    execute_workflow(
+        """
+        [default]
+        depends: R_library("dplyr", autoinstall=True)
+        R:
+            library('dplyr')
+        """
+    )
 
 
-if __name__ == '__main__':
-    unittest.main()
+@pytest.mark.skipif(not shutil.which("Rscript"), reason="R not installed")
+def test_depends_r_library():
+    """Testing depending on R_library"""
+    # first remove xtable package
+    subprocess.call("R CMD REMOVE xtable", shell=True)
+    execute_workflow(
+        """
+        [0]
+
+        depends: R_library('xtable', autoinstall=True)
+        R:
+        library('xtable')
+        ## Demonstrate data.frame
+        tli.table <- xtable(cars)
+        """
+    )
+
+
+@pytest.mark.skipif(not shutil.which("Rscript"), reason="R not installed")
+def test_reexecution():
+    """Test re-execution of steps with R_library"""
+    subprocess.call("R CMD REMOVE xtable", shell=True)
+    wf = """
+    [1]
+    depends: R_library("xtable", autoinstall=True)
+    output: '1.txt'
+    run: expand=True
+        sleep 5
+        touch {_output}
+    """
+    execute_workflow(wf)
+    execute_workflow(wf)
