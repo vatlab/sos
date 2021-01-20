8bb8f1b60dbd87e6ce994b84e2e5650857a3ba10
Bo Peng <ben.bob@gmail.com>
2021-01-19 21:35:19 -0600

    Fix the use of local .sos when docker execution fails #1427

--- src/sos/singularity/client.py
+++ src/sos/singularity/client.py
@@ -203,7 +203,7 @@ class SoS_SingularityClient:
 
             if ret != 0:
                 if script:
-                    debug_script_dir = os.path.join(env.exec_dir, '.sos')
+                    debug_script_dir = os.path.join(os.path.expanduser('~'), '.sos')
                     msg = 'The definition has been saved to {}/singularity.def. To reproduce the error please run:\n``{}``'.format(
                         debug_script_dir, cmd.replace(tempdir,
                                                       debug_script_dir))
@@ -351,7 +351,7 @@ class SoS_SingularityClient:
             ret = self._run_cmd(cmd, **kwargs)
 
             if ret != 0:
-                debug_script_dir = os.path.join(env.exec_dir, '.sos')
+                debug_script_dir = os.path.join(os.path.expanduser('~'), '.sos')
                 msg = 'The script has been saved to {}/{}. To reproduce the error please run:\n``{}``'.format(
                     debug_script_dir, tempscript,
                     cmd.replace(f'{path(tempdir):p}',
