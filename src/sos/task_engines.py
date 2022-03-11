88965f42b3153dce009f0befcbdf0b8d1946bbdd
jenningsje <53481699+jenningsje@users.noreply.github.com>
2022-03-11 15:11:23 -0600

    fix warning: src/sos/task_engines.py:801:16: W0707: Consider explicitly re-raising using the from keyword (raise-missing-from)

--- src/sos/task_engines.py
+++ src/sos/task_engines.py
@@ -799,7 +799,7 @@ class BackgroundProcess_TaskEngine(TaskEngine):
                 job_text += '\n'
             except Exception as e:
                 raise ValueError(
-                    f'Failed to generate job file for task {task_id}: {e}')
+                    f'Failed to generate job file for task {task_id}: {e}') from e
 
         filename = task_ids[0] + ('.sh' if len(task_ids) == 1 else
                                   f'-{task_ids[-1]}.sh')
