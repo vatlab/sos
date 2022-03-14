644a1a2bebb0af56b4ffca014edc7fc1a1c9ffe1
jenningsje <53481699+jenningsje@users.noreply.github.com>
2022-03-14 09:49:05 -0500

    fix warning:src/sos/signatures.py:135:4: R1710: Either all return statements in a function should return an expression, or none of them should. (inconsistent-return-statements)

--- src/sos/signatures.py
+++ src/sos/signatures.py
@@ -140,6 +140,7 @@ class WorkflowSignatures(SignatureDB):
                 f"Failed to write workflow signature of type {entry_type} and id {id}: {e}"
             )
             return None
+        return None
 
     def records(self, workflow_id):
         try:
