a87d21fdde11de87ee7fd0003a3b1300771346f2
jenningsje <53481699+jenningsje@users.noreply.github.com>
2022-03-19 18:40:49 -0500

    fix warning: src/sos/dag.py:296:20: R1720: Unnecessary "else" after "raise" (no-else-raise)

--- src/sos/dag.py
+++ src/sos/dag.py
@@ -301,10 +301,9 @@ class SoS_DAG(nx.DiGraph):
                         raise RuntimeError(
                             f'Completed target {target} is being re-executed. Please report this bug to SoS developers.'
                         )
-                    else:
-                        env.logger.info(
-                            f'Re-running {node._node_id} to generate {target}')
-                        node._status = None
+                    env.logger.info(
+                        f'Re-running {node._node_id} to generate {target}')
+                    node._status = None
             return True
         return False
 
