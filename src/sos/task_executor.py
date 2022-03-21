301c9761a4a1492907a3ef4e9b215afbbb380b08
jenningsje <53481699+jenningsje@users.noreply.github.com>
2022-03-21 15:49:15 -0500

    fix warning: src/sos/targets.py:180:8: R1725: Consider using Python 3 style super() without arguments (super-with-arguments)

--- src/sos/task_executor.py
+++ src/sos/task_executor.py
@@ -401,6 +401,10 @@ class BaseTaskExecutor(object):
         # node. The "n_procs" is the "number of cpus per job per node", which,
         # is "cores" (per task) * "n_workers" (per node). Therefore,
         # "n_procs" should not be used.
+<<<<<<< HEAD
+=======
+        
+>>>>>>> 6b8991269e8f286209973277bb9c0f7bcd7a5c64
         n_nodes, _ = self._parse_num_workers(env.config["worker_procs"])
 
         # regular trunk_workers = ?? (0 was used as default)
