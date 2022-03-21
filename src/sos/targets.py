1b957727aba857d042ed627725c22850957f59fd
jenningsje <53481699+jenningsje@users.noreply.github.com>
2022-03-21 16:39:19 -0500

    fix warning: src/sos/targets.py:1079:8: R1725: Consider using Python 3 style super() without arguments (super-with-arguments)

--- src/sos/targets.py
+++ src/sos/targets.py
@@ -1076,7 +1076,7 @@ class sos_targets(BaseTarget, Sequence, os.PathLike):
         _verify_existence=False,
         **kwargs,
     ):
-        super(sos_targets, self).__init__()
+        super().__init__()
         self._targets: List = []
         self._labels: List = []
         self._groups: List = []
