f38074d760e5d6a8ce5610360e3925454a8654bb
jenningsje <53481699+jenningsje@users.noreply.github.com>
2022-03-24 13:20:25 -0500

    src/sos/targets.py:180:8: R1725: Consider using Python 3 style super() without arguments (super-with-arguments)

--- src/sos/targets.py
+++ src/sos/targets.py
@@ -177,7 +177,7 @@ class sos_variable(BaseTarget):
     """A target for a SoS variable."""
 
     def __init__(self, var):
-        super(sos_variable, self).__init__()
+        super().__init__()
         self._var = var
 
     def target_exists(self, mode="any"):
