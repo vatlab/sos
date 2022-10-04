55e17c8cadb38884b219977233513f798c0d2107
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 05:05:22 +0000

    Adding numeric_owner as keyword arguement

--- src/sos/actions.py
+++ src/sos/actions.py
@@ -1043,7 +1043,7 @@ def downloadURL(URL, dest, decompress=False, index=None):
                             if not is_within_directory(path, member_path):
                                 raise Exception("Attempted Path Traversal in Tar File")
                     
-                        tar.extractall(path, members, numeric_owner) 
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
                         
                     
                     safe_extract(tar, dest_dir)
